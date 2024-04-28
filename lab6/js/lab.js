5dae3fea569e6fdb1337f24697bdace67c2aee91
Wpoundit <166181096+Wpoundit@users.noreply.github.com>
2024-04-28 12:46:52 -0700

    Lab 6
    JS Html and CSS for lab 6

--- lab6/js/lab.js
+++ lab6/js/lab.js
@@ -1,6 +1,25 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// Lab 6
+// Author: Wyatt POund
+// Date: 4/28/2024
+
+// Define Variables
+MyTransport = ["Ford Focus", "Honda Pilot", "Bus"];
+
+// Main Ride Object
+myMainRide = {
+  make : "Ford",
+  model : "Focus",
+  color : "White",
+  year : 2008,
+  age : function() {
+    return 2024 - this.year;
+  }
+}
+
+// Output
+document.writeln("My Main Ride: <pre>", 
+    JSON.stringify(myMainRide, null, '\t'), "</pre>");
+document.writeln("Kinds of transportation I use: ", MyTransport, "</br>");
 
 // Constants
 
